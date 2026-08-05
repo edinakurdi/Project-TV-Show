cacd272bbf1a88db519742979c8d3107fbe279c3
Edina Kurdi <edina.kurdi@gmail.com>
2026-08-05 22:10:51 +0100

    add episode generator function

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+
+  // console.log(allEpisodes);
+  // console.log(allEpisodes[0]);
+
   makePageForEpisodes(allEpisodes);
 }
+
+function makeEpisodeCode(season, episodeNumber) {
+  season = String(season).padStart(2, "0");
+  episodeNumber = String(episodeNumber).padStart(2, "0");
+  const code = "S" + season + "E" + episodeNumber;
+  return code;
+}
+
+// console.log(makeEpisodeCode(2, 7)); // expected: S02E07
+// console.log(makeEpisodeCode(10, 3)); // expected: S10E03
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
