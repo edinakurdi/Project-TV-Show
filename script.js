bdf28c19d570502f7e87044325233b6c0759f471
Edina Kurdi <edina.kurdi@gmail.com>
2026-08-05 23:01:42 +0100

    reorder functions and add comment to function

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+}
+
+//create episode codes
 function makeEpisodeCode(season, episodeNumber) {
   season = String(season).padStart(2, "0");
   episodeNumber = String(episodeNumber).padStart(2, "0");
@@ -17,10 +23,5 @@ function makeEpisodeCode(season, episodeNumber) {
 
 // console.log(makeEpisodeCode(2, 7)); // expected: S02E07
 // console.log(makeEpisodeCode(10, 3)); // expected: S10E03
-
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
 
 window.onload = setup;
