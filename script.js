77a37d6bc0e8e183042c26f2a865f74d405cde92
Edina Kurdi <edina.kurdi@gmail.com>
2026-08-18 10:12:30 +0100

    rename showCard to showTemplate, and template to EpisodeTemplate for consistency

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ const showsEndpoint = "https://api.tvmaze.com/shows";
 // const showSelect = document.getElementById("show-select");
 const episodeSearchInput = document.getElementById("search");
 const episodeSelect = document.getElementById("episode-select");
-const showCard = document.getElementById("show-card");
-const template = document.getElementById("episode-card");
+const showTemplate = document.getElementById("show-card");
+const episodeTemplate = document.getElementById("episode-card");
 const showsView = document.getElementById("shows-view");
 const episodesView = document.getElementById("episodes-view");
 
